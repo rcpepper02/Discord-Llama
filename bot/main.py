d8de3094bf37264a96273b1b5df39b0ca9d94cf2
drxcode <drxcode@outlook.com>
2026-05-20 19:50:16 -0500

    added x feature

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import discord
 import os
 import httpx
-from PIL import Image
+from PIL import Image, ImageOps
 from PIL import ImageFilter
 from PIL import ImageShow
 import random
@@ -70,35 +70,61 @@ async def on_message(message):
                 await message.channel.send(reply)
 
     if "!bingo" in mess:
-        with Image.open("bingo.png") as im:
-            width, height = im.size
-            box_width = width // 5
-            box_height = height // 5
-            boxes = []
-            slices = []
+        processed = mess.split("!bingo", 1)
+        prompt = processed[1].strip()
 
-            for row in range(5):
-                for col in range(5):
-                    box = (
-                        col * box_width,
-                        row * box_height,
-                        (col + 1) * box_width,
-                        (row + 1) * box_height
-                    )
-                    boxes.append(box)
-                    slices.append(im.crop(box))
+        global bingo_msg, boxes, box_width, box_height
 
-            random.shuffle(boxes)
+        if "new" in prompt:
+            with Image.open("bingo.png") as im:
+                width, height = im.size
+                box_width = width // 5
+                box_height = height // 5
+                boxes = []
+                slices = []
 
-            for i in range(len(boxes)):
-                im.paste(slices[i], boxes[i])
+                for row in range(5):
+                    for col in range(5):
+                        box = (
+                            col * box_width,
+                            row * box_height,
+                            (col + 1) * box_width,
+                            (row + 1) * box_height
+                        )
+                        boxes.append(box)
+                        slices.append(im.crop(box))
 
-            
-            im.save("bingo_shuffle.png")
-        
-        with open('bingo_shuffle.png', 'rb') as f:
-            picture = discord.File(f)
-            await message.channel.send(file=picture)
+                random.shuffle(slices)
+
+                for i in range(len(boxes)):
+                    im.paste(slices[i], boxes[i])
+
+                
+                im.save("bingo_shuffle.png")
+    
+            with open('bingo_shuffle.png', 'rb') as f:
+                picture = discord.File(f)
+                bingo_msg = await message.channel.send(file=picture)
+
+        elif "x" in prompt:
+            if bingo_msg is None:
+                await message.channel.send("no bingo board")
+                return
+
+            row = int(prompt[-2])-1
+            col = int(prompt[-1])-1
+
+            grid = [boxes[i:i+5] for i in range(0, len(boxes), 5)]
+
+            bingo = Image.open("bingo_shuffle.png")
+            x = Image.open("cross-x.png")
+            x = ImageOps.contain(x, (box_width, box_height))
+            box = grid[row][col]
+            bingo.paste(x, box[:2], mask=x)
+            bingo.save("bingo_shuffle.png")
+            with open('bingo_shuffle.png', 'rb') as f:
+                picture = discord.File(f)
+                await bingo_msg.edit(attachments=[picture])
 
 clientToken = os.environ.get('goofbot_token')
 #print(API_KEY)
